c44b937bcab90b6551eeccd8725356d57a668a3b
liuzeduo <liuzeduo1234@gmail.com>
2021-06-09 02:44:19 +0800

    [Select] rewrite add [SelectItem]

--- src/components/Select/Select.vue
+++ src/components/Select/Select.vue
@@ -1,100 +1,105 @@
 <template>
   <section class="select">
-    <section class="container">
-      <span class="test">{{ isFocused }}</span>
-      <div
-        class="input-wrapper"
-        @click="handleOpenMenu"
-        @mouseenter="handleHoverEnter"
-        @mouseleave="handleHoverLeave"
+    <span class="test">{{ isFocused }}</span>
+    <div
+      class="input-wrapper"
+      @mousedown="handleWannaSelect"
+      @mouseenter="handleHoverEnter"
+      @mouseleave="handleHoverLeave"
+    >
+      <input
+        type="text"
+        :id="id"
+        ref="input"
+        v-model="valueSelected"
+        @focus="handleFocus"
+        @blur="handleBlur"
+      />
+      <label
+        :for="id"
+        v-if="labelDisplayed"
+        :style="{
+          color: computedLabelColor,
+          transform: computedLabelPosition,
+        }"
+        >{{ computedLabelText }}</label
       >
-        <input
-          type="text"
-          :id="id"
-          ref="input"
-          v-model="valueSelected"
-          @focus="handleFocus"
-          @blur="handleBlur"
-        />
-        <label
-          :for="id"
-          v-if="labelDisplayed"
-          :style="{ color: computedLabelColor ,transform:computedLabelPosition}"
-          >{{ computedLabelText }}</label
-        >
-        <span
-          class="selected-value"
-          :style="{ color: computedSelectedColor }"
-          >{{ computedDisplayedValue }}</span
-        >
-        <span class="synchronizing-trick-activeindex" v-show="false">{{
-          activeIndex
-        }}</span>
-        <span
-          class="expand-action"
-          :class="!isMenuOpened ? 'towards-down' : 'towards-up'"
-        >
-          <Icon
-            name="down"
-            size="md"
-            :class="disabled ? 'disabled' : ''"
-          ></Icon>
-        </span>
-        <span
-          class="base-underline"
-          :style="{ borderBottom: computedBaseUnderlineBorder }"
-        ></span>
-        <span
-          class="focus-underline"
-          :style="{ transform: computedFocusUnderlineScale }"
-        ></span>
-      </div>
-      <div
-        class="input-helper"
-        v-if="helper"
-        :class="disabled ? 'disabled' : ''"
+      <span class="selected-value" :style="{ color: computedSelectedColor }">{{
+        computedDisplayedValue
+      }}</span>
+      <span class="synchronizing-trick-activeindex" v-show="false">{{
+        activeIndex
+      }}</span>
+      <span
+        class="expand-action"
+        :class="menuToggled ? 'towards-up' : 'towards-down'"
       >
-        <span class="text" :style="{ color: computedHelperColor }">{{
-          helper
-        }}</span>
+        <Icon name="down" size="md" :class="disabled ? 'disabled' : ''"></Icon>
+      </span>
+      <span
+        class="base-underline"
+        :style="{ borderBottom: computedBaseUnderlineBorder }"
+      ></span>
+      <span
+        class="focus-underline"
+        :style="{ transform: computedFocusUnderlineScale }"
+      ></span>
+    </div>
+    <div class="input-helper" v-if="helper" :class="disabled ? 'disabled' : ''">
+      <span class="text" :style="{ color: computedHelperColor }">{{
+        helper
+      }}</span>
+    </div>
+    <transition name="fade-from-left">
+      <div class="input-menu" v-if="menuToggled" :style="menuPos">
+        <div class="control-mask" @mousedown="handleStopSelect"></div>
+        <div class="item-wrapper item-none"></div>
+        <div class="item-wrapper" v-for="(item, idx) in items" :key="idx">
+          <select-item
+            :label="item.label"
+            :text="item.text"
+            :index="idx"
+            :selected="item.label === cValue"
+            @select="handleDoneSelect"
+          ></select-item>
+        </div>
       </div>
-      <transition name="left-fade">
-        <div class="menu" v-if="isMenuOpened" :style="menuPos">
-          <div
-            class="control-mask"
-            v-if="isMaskOpened"
-            @mousedown="handleCloseMask"
-          ></div>
-          <div
-            class="outer"
-            v-for="(item, idx) in itemsFiltered"
-            :key="idx"
-            @click="handleClick"
-            :class="valueSelected === item.label ? 'selected' : ''"
+    </transition>
+    <!-- <div
+            class="item"
+            :style="{}"
+            @mousedown="handleDoneSelect($event, item.label, idx)"
           >
-            <anchor
-              :label="item.label"
-              :icon="item.icon | sizeAdder('md')"
-              @select="handleSelect(idx, $event)"
-            >
-              <template>{{ item.text }}</template>
-            </anchor>
+            <span class="text">{{ item.text }}</span>
           </div>
-        </div></transition
-      >
-    </section>
+        </div> -->
+    <!-- <div
+          class="outer"
+          v-for="(item, idx) in itemsFiltered"
+          :key="idx"
+          @click="handleClick"
+          :class="valueSelected === item.label ? 'selected' : ''"
+        >
+          <anchor
+            :label="item.label"
+            :icon="item.icon | sizeAdder('md')"
+            @select="handleSelect(idx, $event)"
+          >
+            <template>{{ item.text }}</template>
+          </anchor>
+        </div> -->
   </section>
 </template>
 <script>
 import Icon from "../Icon/Icon.vue";
-import Navigable from "../../mixins/navigable";
 import Rippleable from "../../mixins/rippleable";
+import SelectItem from "./SelectItem.vue";
 //todo flexible width responding to selected-value
 export default {
   name: "Select",
   props: {
     items: {
-      //[{label:'...'}]
+      //[ { label: "twenty", text: "Twenty" },]
       type: Array,
     },
     activeIndex: {
@@ -154,18 +159,16 @@ export default {
   },
   components: {
     Icon,
+    SelectItem,
   },
-  mixins: [Navigable, Rippleable],
+  mixins: [Rippleable],
   data() {
     return {
       id: null,
       isHovering: false,
       isFocused: false,
-      // valueSelected: null,
       cValue: null,
-      //menu
-      isMenuOpened: false,
-      isMaskOpened: false,
+      menuToggled: false,
     };
   },
   mounted() {
@@ -192,11 +195,13 @@ export default {
       else if (this.isFocused) return "rgba(25, 118, 210, 1)";
       else return this.labelColor || "rgba(0, 0, 0, 0.54)";
     },
-    computedLabelPosition(){
-      const floatingEffect="translateY(calc(-100% - 6px)) translateX(-10%) scale(0.8)"
+    computedLabelPosition() {
+      const floatingEffect =
+        "translateY(calc(-100% - 6px)) translateX(-10%) scale(0.8)";
       //when the input is focused or the input is valued or the input is placeholdered
-      if(this.isFocused||this.cValue||this.placeholder)return floatingEffect
-      return null
+      if (this.isFocused || this.cValue || this.placeholder)
+        return floatingEffect;
+      return null;
     },
     computedHelperColor() {
       if (this.disabled) return "rgba(0, 0, 0, 0.38)";
@@ -247,6 +252,14 @@ export default {
       console.log("handleHoverLeave");
       this.isHovering = false;
     },
+    handleFocus() {
+      //console.log("handleFocus");
+      if (!this.disabled) this.isFocused = true;
+    },
+    handleBlur() {
+      //console.log("handleBlur");
+      if (!this.disabled) this.isFocused = false;
+    },
     initValue() {
       //todo safety check
       if (this.activeIndex !== undefined) {
@@ -260,241 +273,176 @@ export default {
     handleClick(e) {
       this.createRipple(e, false, "default");
     },
-    handleSelect(index, e) {
-      if (this.disabled) return;
-      let label = e;
-      console.log("handleSelect", index, label);
-      if (label === "$special") {
-        //None -> clear the selection
-        this.cValue = null;
-        this.$refs.input.blur();
-        this.$emit("change", { label: null });
-      } else {
-        this.$refs.input.focus();
-        this.cValue = label;
-      }
-      this.reportChange(label, index);
-      this.openMenu(false);
-    },
-    reportChange(label, index) {
-      //the 0 is None added later
-      this.$emit("change", { label: label, index: index });
-    },
-    handleFocus() {
-      //console.log("handleFocus");
-      if (!this.disabled) this.isFocused = true;
-    },
-    handleBlur() {
-      //console.log("handleBlur");
-      if (!this.disabled) this.isFocused = false;
-    },
-    openMenu(opened) {
-      console.log("openMenu", opened);
-      this.isMenuOpened = opened;
-      this.isMaskOpened = opened;
-    },
-    handleOpenMenu() {
+    handleWannaSelect() {
       if (this.disabled || this.readonly) return;
-      this.openMenu(true);
+      this.menuToggled = true;
       this.$refs.input.focus();
     },
-    handleCloseMask() {
+    handleStopSelect() {
+      //by clicking control-mask
+      this.menuToggled = false;
+    },
+    handleDoneSelect(e) {
+      if (this.disabled) return;
+      let {label,index}=e
+      console.log("handleSelectDone", label, index);
       this.$refs.input.focus();
-      this.openMenu(false);
+      this.cValue = label;
+      this.menuToggled = false;
+      this.$emit("change", { label, index });
     },
   },
 };
 </script>
 <style lang="less" scoped>
 .select {
-  .container {
+  position: relative;
+  width: 120px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  margin-top: 16px;
+  // max-width: 120px;
+  .test {
+    position: absolute;
+    right: 200%;
+  }
+  .input-helper {
+    //helpful description text below the input
+    width: 100%;
+    margin-top: 3px;
+    word-wrap: break-word;
+    word-break: break-all;
+    .text {
+      font-size: 12px;
+      font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+      font-weight: 400;
+      letter-spacing: 0.03333em;
+    }
+  }
+  .input-wrapper {
+    width: 100%;
     position: relative;
-    width: 120px;
     display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    margin-top: 16px;
-    // max-width: 120px;
-    .test {
-      position: absolute;
-      right: 200%;
-    }
-    .input-helper {
-      //helpful description text below the input
+    cursor: pointer;
+    input {
       width: 100%;
-      margin-top: 3px;
-      word-wrap: break-word;
-      word-break: break-all;
-      .text {
-        font-size: 12px;
-        font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-        font-weight: 400;
-        letter-spacing: 0.03333em;
-      }
-    }
-    .input-wrapper {
-      width: 100%;
-      position: relative;
-      display: flex;
+      height: 32px;
+      padding: 6px 24px 7px 0;
+      box-sizing: border-box;
+      border: none;
       cursor: pointer;
-      input {
-        width: 100%;
-        height: 32px;
-        padding: 6px 24px 7px 0;
-        box-sizing: border-box;
-        border: none;
-        cursor: pointer;
-        color: transparent; //hide focus cursor and value
-        user-select: none;
-      }
-      .hide-selection {
-        //todo hide the value of input when selecting
-        background: transparent;
-        color: transparent;
-      }
-      input::selection {
+      color: transparent; //hide focus cursor and value
+      user-select: none;
+    }
+    .hide-selection {
+      //todo hide the value of input when selecting
+      background: transparent;
+      color: transparent;
+    }
+    input::selection {
+      .hide-selection();
+    }
+    input:focus {
+      outline: none;
+      &::selection {
         .hide-selection();
       }
-      input:focus {
-        outline: none;
-        &::selection {
-          .hide-selection();
-        }
-      }
-      // .floating-effect {
-      //   transform: translateY(calc(-100% - 6px)) translateX(-10%) scale(0.8);
-      // }
-      // input:focus + label {
-      //   color: #1976d2;
-      //   .floating-effect();
-      // }
-      // input:not(:placeholder-shown) + label {
-      //   .floating-effect();
-      // }
-      label {
-        position: absolute;
-        top: 6px;
-        transition: color 200ms cubic-bezier(0, 0, 0.2, 1) 0ms,
-          transform 200ms cubic-bezier(0, 0, 0.2, 1) 0ms;
-        font-family: "Roboto", "Helvetica";
-        font-size: 16px;
-        line-height: 16px;
-        text-transform: capitalize;
-        user-select: none;
-      }
+    }
+    label {
+      position: absolute;
+      top: 6px;
+      transition: color 200ms cubic-bezier(0, 0, 0.2, 1) 0ms,
+        transform 200ms cubic-bezier(0, 0, 0.2, 1) 0ms;
+      font-family: "Roboto", "Helvetica";
+      font-size: 16px;
+      line-height: 16px;
+      text-transform: capitalize;
+      user-select: none;
+    }
 
-      .selected-value {
-        position: absolute;
-        top: 6px;
-        color: rgba(0, 0, 0, 0.87);
-        font-family: "Roboto", "Helvetica";
-        font-size: 16px;
-        line-height: 18px;
-        text-transform: capitalize;
-        cursor: pointer;
-        user-select: none;
+    .selected-value {
+      position: absolute;
+      top: 6px;
+      color: rgba(0, 0, 0, 0.87);
+      font-family: "Roboto", "Helvetica";
+      font-size: 16px;
+      line-height: 18px;
+      text-transform: capitalize;
+      cursor: pointer;
+      user-select: none;
+      &.disabled {
+        color: rgba(0, 0, 0, 0.38);
+      }
+    }
+    .underline {
+      position: absolute;
+      width: 100%;
+      bottom: 0;
+    }
+    .base-underline {
+      .underline();
+      transition: border-bottom-color 200ms cubic-bezier(0.4, 0.2, 0, 1);
+    }
+    .focus-underline {
+      .underline();
+      transition: transform 200ms cubic-bezier(0.4, 0.2, 0, 1);
+      border-bottom: 2px solid #1976d2;
+    }
+    .expand-action {
+      position: absolute;
+      top: 4px;
+      right: 0;
+      .icon {
+        width: 24px;
+        height: 24px;
         &.disabled {
           color: rgba(0, 0, 0, 0.38);
         }
       }
-      .underline {
-        position: absolute;
-        width: 100%;
-        bottom: 0;
+      cursor: pointer;
+      transition: transform 100ms cubic-bezier(0.4, 0.2, 0, 1);
+      &.towards-down {
+        transform: rotate(0);
       }
-      .base-underline {
-        .underline();
-        transition: border-bottom-color 200ms cubic-bezier(0.4, 0.2, 0, 1);
-      }
-      .focus-underline {
-        .underline();
-        transition: transform 200ms cubic-bezier(0.4, 0.2, 0, 1);
-        border-bottom: 2px solid #1976d2;
-      }
-      .expand-action {
-        position: absolute;
-        top: 4px;
-        right: 0;
-        .icon {
-          width: 24px;
-          height: 24px;
-          &.disabled {
-            color: rgba(0, 0, 0, 0.38);
-          }
-        }
-        cursor: pointer;
-        transition: transform 100ms cubic-bezier(0.4, 0.2, 0, 1);
-        &.towards-down {
-          transform: rotate(0);
-        }
-        &.towards-up {
-          transform: rotate(180deg);
-        }
-      }
-    }
-
-    .menu {
-      position: absolute;
-      box-shadow: 0px 5px 5px -3px rgba(0, 0, 0, 0.2),
-        0px 8px 10px 1px rgba(0, 0, 0, 0.14),
-        0px 3px 14px 2px rgba(0, 0, 0, 0.12);
-      background: #fff;
-      padding: 8px 0;
-      min-width: 120px;
-      z-index: 10;
-      .control-mask {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        z-index: -10;
-      }
-      .outer {
-        &:hover {
-          background-color: rgba(0, 0, 0, 0.04);
-        }
-        &.selected {
-          background-color: rgba(0, 0, 0, 0.08);
-        }
-        display: flex;
-        align-items: center;
-        transition: background-color 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-        .anchor {
-          width: 100%;
-          height: 36px;
-          padding: 6px 16px;
-          box-sizing: border-box;
-          font-size: 16px;
-          font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-          color: rgba(0, 0, 0, 0.87);
-          transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-          cursor: pointer;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-        }
+      &.towards-up {
+        transform: rotate(180deg);
       }
     }
   }
+  .input-menu {
+    position: absolute;
+    box-shadow: 0px 5px 5px -3px rgba(0, 0, 0, 0.2),
+      0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12);
+    background: #fff;
+    padding: 8px 0;
+    //todo responsive width
+    min-width: 120px;
+    z-index: 10;
+    .control-mask {
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      z-index: -10;
+    }
+    .item-wrapper {
+      display: flex;
+    }
+  }
 }
-.fade {
-  transition: opacity 251ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
-    transform 167ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-}
-.fade-action {
-  transform: scale(0);
-  opacity: 0;
-}
-.left-fade {
+.fade-from-left {
   &-enter-active,
   &-leave-active {
-    .fade();
+    transition: opacity 251ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+      transform 167ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
     transform-origin: 0px 26px;
   }
   &-leave-to,
   &-enter {
-    .fade-action();
+    transform: scale(0);
+    opacity: 0;
   }
 }
 </style>
